04b14b9a47ff619e677d9aa79ed389a8e0132711
Max Booth <maxwell.booth@gmail.com>
2018-07-12 04:42:20 -0400

    making it better than in the book :)

--- c2fi.rb
+++ c2fi.rb
@@ -1,6 +1,4 @@
 print "Hello. Please enter a Celsius value: "
 celsius = gets
 fahrenheit = (celsius.to_i * 9 / 5) + 32
-print "The Fahrenheit equivalent is "
-print fahrenheit
-puts "."
+puts "The Fahrenheit equivalent is #{fahrenheit.to_s}."
